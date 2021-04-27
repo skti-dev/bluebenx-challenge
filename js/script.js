99d670a2b165b54609dc6e1bc44a52bcb5c4360d
Augusto Seabra <augusto.seabra00@gmail.com>
2021-04-26 22:06:22 -0300

    Ajustando javascript e semantica do html

--- js/script.js
+++ js/script.js
@@ -1,22 +1,30 @@
+const form = document.querySelector("#generic-form")
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+  form.reset()
+})
+
 const showPassword = document.querySelector("#show-password"),
 hidePassword = document.querySelector("#hide-password")
 
-const toggleIconVisibility = () => {
-  showPassword.classList.toggle("d-none")
-  hidePassword.classList.toggle("d-none")
-}
-
-const togglePasswordVisibility = () => {
-  const passwordInput = document.querySelector("#password")
-  
-  if(passwordInput.type === "password") {
-    passwordInput.type = "text"
-  }else{
-    passwordInput.type = "password"
+if(showPassword && hidePassword) {
+  const toggleIconVisibility = () => {
+    showPassword.classList.toggle("d-none")
+    hidePassword.classList.toggle("d-none")
   }
-
-  toggleIconVisibility()
+  
+  const togglePasswordVisibility = () => {
+    const passwordInput = document.querySelector("#password")
+    
+    if(passwordInput.type === "password") {
+      passwordInput.type = "text"
+    }else{
+      passwordInput.type = "password"
+    }
+  
+    toggleIconVisibility()
+  }
+  
+  showPassword.addEventListener("click", togglePasswordVisibility)
+  hidePassword.addEventListener("click", togglePasswordVisibility)
 }
-
-showPassword.addEventListener("click", togglePasswordVisibility)
-hidePassword.addEventListener("click", togglePasswordVisibility)

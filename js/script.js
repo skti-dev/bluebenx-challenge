58d12c859dd17da4e2d969192a8ed3683133a2ac
Augusto Seabra <augusto.seabra00@gmail.com>
2021-04-28 23:08:02 -0300

    recebendo valores do formulario no js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,42 @@
-const form = document.querySelector("#generic-form")
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
-  
-  if(form.getAttribute("action") != "#") window.location.href = form.action
-})
+const isFormValid = form => {
+  const formData = new FormData(form)
 
-form.addEventListener("reset", (event) => {
-  if(confirm("Você tem certeza que deseja limpar todos dados do formulário?")) {
-    return true
-  }else{
-    event.preventDefault()
+  let finalFormData = {}
+  for (var key of formData.keys()) {
+    finalFormData[key] = formData.get(key)
   }
-})
+
+  console.log("Objeto final: ")
+  console.table(finalFormData)
+
+  return true
+}
+
+const loginForm = document.querySelector("#login-form")
+if(loginForm) {
+  loginForm.addEventListener("submit", event => {
+    event.preventDefault()
+    if(isFormValid(loginForm)) loginForm.reset()
+  })
+}
+
+const registerForm = document.querySelector("#register-form")
+if(registerForm) {
+  registerForm.addEventListener("submit", event => {
+    event.preventDefault()
+    if(isFormValid(registerForm)) {
+      if(window.confirm("A tela será redirecionada, deseja prosseguir?")) window.location.href = registerForm.action
+    }
+  })
+  
+  registerForm.addEventListener("reset", event => {
+    if(window.confirm("Você deseja limpar os campos do formulário?")) return true
+    event.preventDefault()
+  })
+}
 
 const showPassword = document.querySelector("#show-password"),
 hidePassword = document.querySelector("#hide-password")
-
 if(showPassword && hidePassword) {
   const toggleIconVisibility = () => {
     showPassword.classList.toggle("d-none")
